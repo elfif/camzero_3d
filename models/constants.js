c15fb53c606421ea8d0e60db8f55384928e6aa82
FiFoOoO <fifoooo@gmail.com>
2026-07-18 19:53:14 +0200

    chore: test for 2mm joint with 5mm wall thickness

--- models/constants.js
+++ models/constants.js
@@ -11,7 +11,7 @@ const threadSegmentsPerRotation = Math.max(8, Math.round((segments * 48) / 128))
 
 const camModel = requireCamModelFromEnv();
 
-const wallThickness = 6;
+const wallThickness = 5;
 const roundedRadius = 8;
 const largeRoundedRadius = 10;
 
